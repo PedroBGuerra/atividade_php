eef4b321a003d1cd9fd4a1136a15e3b84f565ad3
PedroBGuerra <pedroguerrabrejao2@gmail.com>
2025-03-25 22:18:43 -0300

    atividade

---
+++ ex01.php
@@ -0,0 +1,18 @@
+<?php
+
+$num1 = 10;
+$num2 = 10;
+$resul = $num1 + $num2;
+
+
+if ($resul > 20) {
+
+	echo $resul + 8;
+
+}elseif ($resul <= 20) {
+
+	echo ($resul - 5);
+}
+
+
+?>
---
+++ ex02.php
@@ -0,0 +1,36 @@
+<?php
+ 
+$n1 = 20.0;
+$n2= 45.0;
+$n3 = 82.00;
+ 
+ 
+if($n1%2==0) {
+	echo "divisível por 2 <br/>";
+ 
+}
+elseif ($n1%5==0) {
+	echo "divisível por 5";
+ 
+}
+elseif ($n1%10==0) {
+ 
+	echo "divisível por 10";
+}
+ 
+ 
+ 
+if( $n2%2==0) {
+
+	echo "divisível por 2";
+ 
+}
+elseif ($n2%5==0) {
+
+	echo "divisível por 5 <br/>";
+ 
+}
+elseif ($n2%10==0) {
+ 
+	echo "divisível por 10 <br/>";
+}
