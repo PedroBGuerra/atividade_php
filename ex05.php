4e8eac8cd9d23b9a8d087a5d08ddf1f7247ef0d7
PedroBGuerra <pedroguerrabrejao2@gmail.com>
2025-04-01 22:13:47 -0300

    Add files via upload

---
+++ ex05.php
@@ -0,0 +1,64 @@
+<?php
+
+$num1 =1;
+$altura = 1.78;
+$idade = 25;
+$sexo = "m";
+
+$num1 =2;
+$altura = 1.80;
+$idade = 30;
+$sexo = "f";
+
+$num1 =3;
+$altura = 1.78;
+$idade = 25;
+$sexo = "m";
+
+$num1 =4;
+$altura = 1.78;
+$idade = 25;
+$sexo = "f";
+
+$num1 =5;
+$altura = 1.78;
+$idade = 25;
+$sexo = "m";
+
+$num1 =6;
+$altura = 1.78;
+$idade = 25;
+$sexo = "f";
+
+echo ($maiorAltura = 0
+$menorAltura = 999
+$somaAlturaMulheres = 0
+$qtdMulheres = 0
+$idadeHomemMaisVelho = 0
+$idadeMulherMaisNova = 999)
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+?>
